81086cf548d6d93adf195ed4ac44d5d13f206a41
Rafael Santos <rafa.05125@gmail.com>
2023-08-23 23:07:18 -0300

    Google fonts

--- App.tsx
+++ App.tsx
@@ -2,8 +2,27 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import { Home } from "./src/pages/Home";
 import theme from "./src/global/styles/theme";
+import * as SplashScreen from "expo-splash-screen";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 const App: React.FunctionComponent = () => {
+  SplashScreen.preventAutoHideAsync();
+
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  SplashScreen.hideAsync();
+
   return (
     <ThemeProvider theme={theme}>
       <Home />

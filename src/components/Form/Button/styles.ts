f09b706bd994339f0644a6e548b4e886da6e9307
Rafael Santos <rafa.05125@gmail.com>
2023-08-28 08:59:43 -0300

    Button component inital style

--- src/components/Form/Button/styles.ts
+++ src/components/Form/Button/styles.ts
@@ -1,6 +1,23 @@
 import { TouchableOpacity } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
-export const Container = styled(TouchableOpacity)``;
+export const Container = styled(TouchableOpacity)`
+  width: 100%;
+  align-items: center;
+  border-radius: 5px;
+  padding: 18px;
+  margin-top: ${RFValue(16)}px;
 
-export const Title = styled.Text``;
+  background-color: ${(props: { theme: { colors: { primary: string } } }) =>
+    props.theme.colors.primary};
+`;
+
+export const Title = styled.Text`
+  font-size: ${RFValue(14)}px;
+
+  font-family: ${(props: { theme: { fonts: { bold: string } } }) =>
+    props.theme.fonts.bold};
+  color: ${(props: { theme: { colors: { dark: string } } }) =>
+    props.theme.colors.dark};
+`;

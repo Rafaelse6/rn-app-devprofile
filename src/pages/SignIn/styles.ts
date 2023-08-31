1816547efbd168979f2a819065db33d4c0eb76c6
Rafael Santos <rafa.05125@gmail.com>
2023-08-31 20:00:44 -0300

    SignUp page access button

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components/native";
 import { RFValue } from "react-native-responsive-fontsize";
+import { Feather } from "@expo/vector-icons";
 
 export const Container = styled.View`
   flex: 1;
@@ -41,4 +42,35 @@ export const ForgotPasswordTitle = styled.Text`
     props.theme.fonts.regular};
   color: ${(props: { theme: { colors: { gray500: string } } }) =>
     props.theme.colors.gray500};
+`;
+
+export const Icon = styled(Feather)`
+  font-size: ${RFValue(20)}px;
+  color: ${(props: { theme: { colors: { primary: string } } }) =>
+    props.theme.colors.primary};
+`;
+
+export const CreateAccount = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  border-top-width: 1px;
+  padding: 16px 0;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  border-color: ${(props: { theme: { colors: { black: string } } }) =>
+    props.theme.colors.black};
+  background-color: ${(props: { theme: { colors: { gray800: string } } }) =>
+    props.theme.colors.gray800};
+`;
+
+export const CreateAccountTitle = styled.Text`
+  margin-left: 16px;
+  font-size: ${RFValue(18)}px;
+  font-family: ${(props: { theme: { fonts: { regular: string } } }) =>
+    props.theme.fonts.regular};
+  color: ${(props: { theme: { colors: { primary: string } } }) =>
+    props.theme.colors.primary};
 `;

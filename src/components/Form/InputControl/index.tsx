bf8ee5262e3fac4a4712708c13789c78e1286936
Rafael Santos <rafa.05125@gmail.com>
2023-09-10 22:30:25 -0300

    InputControl refactor

--- src/components/Form/InputControl/index.tsx
+++ src/components/Form/InputControl/index.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { Control, Controller } from "react-hook-form";
-import { Container } from "./styles";
+import { Container, Error } from "./styles";
 import { TextInputProps } from "react-native";
 import { Input } from "../Input";
 
 interface Props extends TextInputProps {
   control: Control;
   name: string;
+  error: string | undefined;
 }
 
 export const InputControl: React.FunctionComponent<Props> = ({
   control,
   name,
+  error,
   ...otherProps
 }) => {
   return (
@@ -28,6 +30,7 @@ export const InputControl: React.FunctionComponent<Props> = ({
         )}
         name={name}
       />
+      {error && <Error>{error}</Error>}
     </Container>
   );
 };
--- src/components/Form/InputControl/styles.ts
+++ src/components/Form/InputControl/styles.ts
@@ -1,5 +1,15 @@
+import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
   width: 100%;
+`;
+
+export const Error = styled.Text`
+  font-size: ${RFValue(14)}px;
+  margin-bottom: 16px;
+  color: ${(props: { theme: { colors: { danger: string } } }) =>
+    props.theme.colors.danger};
+  font-family: ${(props: { theme: { fonts: { regular: string } } }) =>
+    props.theme.fonts.regular};
 `;

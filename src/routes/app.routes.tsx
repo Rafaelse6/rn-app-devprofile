9fa058737fbfc69478d6c37992a006ddd98527c8
Rafael Santos <rafa.05125@gmail.com>
2023-10-31 00:20:38 -0300

    Reset password route

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -4,6 +4,7 @@ import { Home } from "../pages/Home";
 import { UserDetails } from "../pages/UserDetails";
 import { UserProfile } from "../pages/UserProfile";
 import { UserProfileEdit } from "../pages/UserProfileEdit/Index";
+import { UserProfilePassword } from "../pages/UserProfilePassword/Index";
 
 const App = createNativeStackNavigator();
 
@@ -17,6 +18,7 @@ export const AppRoutes: React.FunctionComponent = () => {
       <App.Screen name="UserDetails" component={UserDetails} />
       <App.Screen name="UserProfile" component={UserProfile} />
       <App.Screen name="UserProfileEdit" component={UserProfileEdit} />
+      <App.Screen name="UserProfilePassword" component={UserProfilePassword} />
     </App.Navigator>
   );
 };
